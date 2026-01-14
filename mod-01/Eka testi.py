c0a79b5b211a4908b32ab613899fbf40b2982c8b
Rehman <rehman.ahmad2@metropolia.fi>
2026-01-14 10:43:23 +0200

    Teht palautus

---
+++ mod-01/Eka testi.py
@@ -0,0 +1,5 @@
+nimi = input("Anna nimesi: ")
+print("Terve,", nimi + "!")
+
+sade = float(input("Säde: "))
+print(3.14 * sade * sade)
---
+++ mod-01/python tehtävä 01.py
@@ -0,0 +1,2 @@
+sade = float(input("Säde: "))
+print(3.14 * sade * sade)
