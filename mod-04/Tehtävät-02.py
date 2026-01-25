e765538626102cb8f6cd2a5b6d9b7065356281c1
Rehman <rehman.ahmad2@metropolia.fi>
2026-01-25 20:22:14 +0200

    Ohejelmisto 1 tehtävä luku 4 tehtävät

---
+++ mod-04/Tehtävät-02.py
@@ -0,0 +1,6 @@
+tuuma = 0
+while tuuma >= 0:
+    tuuma = float(input("Anna tuumat: "))
+    if tuuma >= 0:
+        cm = tuuma * 2.54
+        print(cm, "cm")
---
+++ mod-04/Tehtävät-03.py
@@ -0,0 +1,7 @@
+luku = input ("Anna luku: ")
+while luku !="":
+    if luku == "":
+        break
+    print ("Suoritan toiminnon: " + luku)
+    luku = input("Anna luku: ")
+print ("Toiminnot lopetettu.")
