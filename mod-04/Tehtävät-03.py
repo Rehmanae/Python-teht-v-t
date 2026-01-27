c12c90e6bd282e13cf513314c27bd0d6f0a4f953
Rehman <rehman.ahmad2@metropolia.fi>
2026-01-27 21:19:52 +0200

    Ohejelmisto 1 tehtävä luku 4 tehtävät

--- mod-04/Tehtävät-03.py
+++ mod-04/Tehtävät-03.py
@@ -1,7 +1,28 @@
-luku = input ("Anna luku: ")
-while luku !="":
-    if luku == "":
-        break
-    print ("Suoritan toiminnon: " + luku)
-    luku = input("Anna luku: ")
-print ("Toiminnot lopetettu.")
+import math
+
+pienin = math.inf
+suurin = -math.inf
+
+syote = input("Anna luku: ")
+
+while syote != "":
+    luku = int(syote)
+
+    if luku < pienin:
+        pienin = luku
+    if luku > suurin:
+        suurin = luku
+
+    syote = input("Anna luku: ")
+
+# Tässä koodi laskee, kumpu luku on pienin ja suurin.
+if pienin != math.inf:
+    print(f"Pienin: {pienin}")
+    print(f"Suurin: {suurin}")
+else:
+    print("Et antanut yhtään lukua")
+
+
+
+
+
