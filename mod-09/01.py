399d715667c7d503b9bc30c24de17426185394f3
Rehman <rehman.ahmad2@metropolia.fi>
2026-03-17 14:00:54 +0200

    Eka tehtävä

--- mod-09/01.py
+++ mod-09/01.py
@@ -1,9 +1,26 @@
-class Koira:
-    pass
+"""
+Kirjoita Auto-luokka, jonka ominaisuuksina ovat rekisteritunnus, huippunopeus, tämänhetkinen nopeus ja kuljettu matka.
+ Kirjoita luokkaan alustaja, joka asettaa ominaisuuksista kaksi ensin mainittua parametreina saatuihin arvoihin.
+  Uuden auton nopeus ja kuljetut matka on asetettava automaattisesti nollaksi.
+  Kirjoita pääohjelma, jossa luot uuden auton (rekisteritunnus ABC-123, huippunopeus 142 km/h).
+  Tulosta pääohjelmassa sen jälkeen luodun auton kaikki ominaisuudet.
+
+"""
+
+class Auto:
+    def __init__(self, rekisteritunnus, huippunopeus, tämänhetkinen_nopeus, kuljettu_matka):
+        self.tamanhetkinen_nopeus = tämänhetkinen_nopeus
+        self.kuljettu_matka = kuljettu_matka
+        self.rekisteritunnus = rekisteritunnus
+        self.huippunopeus = huippunopeus
 
 
-koira = Koira()
-koira.nimi = "Rekku"
-koira.syntymävuosi = 2022
 
-print(f"{koira.nimi} on syntynyt vuonna {koira.syntymävuosi}.")
+
+auto = Auto("ABC-123","142 km/h", 0,0 )
+print(f"Auton rekisteritunnus on {auto.rekisteritunnus}")
+print(f"Auton huippunopeus on {auto.huippunopeus}")
+print(f"Auton kuljettu matka on {auto.kuljettu_matka}")
+print(f"Auton tämänhetkinen nopeus on {auto.tamanhetkinen_nopeus}")
+
+
