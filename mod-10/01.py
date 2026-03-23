1606bbb312c20e789a5aa71ab927afcdfc9f678d
Rehman <rehman.ahmad2@metropolia.fi>
2026-03-23 16:36:27 +0200

    Tehtävä 1

--- mod-10/01.py
+++ mod-10/01.py
@@ -1,3 +1,14 @@
+"""
+Kirjoita Hissi-luokka, joka saa alustajaparametreinaan alimman ja ylimmän kerroksen numeron. Hissillä on metodit siirry_kerrokseen,
+kerros_ylös ja kerros_alas. Uusi hissi on aina alimmassa kerroksessa. Jos tee luodulle hissille h esimerkiksi metodikutsun h.siirry_
+kerrokseen(5), metodi kutsuu joko kerros_ylös- tai kerros_alas-metodia niin monta kertaa, että hissi päätyy viidenteen kerrokseen.
+Viimeksi mainitut metodit ajavat hissiä yhden kerroksen ylös- tai alaspäin ja ilmoittavat, missä kerroksessa hissi sen jälkeen on.
+Testaa luokkaa siten, että teet pääohjelmassa hissin ja käsket sen siirtymään haluamaasi kerrokseen ja sen jälkeen takaisin alimpaan
+kerrokseen.
+"""
+
+
+
 class Hissi:
     def __init__(self, alin_kerros, ylin_kerros):
         self.alin = alin_kerros
